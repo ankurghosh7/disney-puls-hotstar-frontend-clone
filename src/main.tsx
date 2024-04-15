51a63d2048f3c94792b106136ab4328f89d88a34
ankur ghosh <ankurghosh041@gmail.com>
2024-04-15 20:12:03 +0530

    update code

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,7 @@ import "./index.css";
 import { RouterProvider } from "react-router-dom";
 import { BrowserRouter } from "./routes.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
@@ -17,7 +17,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
   <QueryClientProvider client={queryClient}>
     <RouterProvider router={BrowserRouter} />
-    {/* <ReactQueryDevtools initialIsOpen={false} /> */}
+    <ReactQueryDevtools initialIsOpen={false} />
   </QueryClientProvider>
   </React.StrictMode>
 );
